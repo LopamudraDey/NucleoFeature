c2e5f8e6950252480d33b6b92194ac4ecc03ff6b
LopamudraDey <50617261+LopamudraDey@users.noreply.github.com>
2025-02-26 15:26:47 +0100

    Update gc.R

--- R/gc.R
+++ R/gc.R
@@ -1,5 +1,5 @@
 gc_content <- function(sequence_in) {
-  sequence_in <- as.character(sequence_in)
+  #sequence_in <- as.character(sequence_in)
   sequence_in <- toupper(unlist(strsplit(sequence_in, "")))
   gc <- sum(sequence_in == "G" | sequence_in == "C") / length(sequence_in) * 100
   return(gc)
