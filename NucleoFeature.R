6a0c8988938caadd7115411dc0505b6093249a07
LopamudraDey <50617261+LopamudraDey@users.noreply.github.com>
2025-03-25 15:15:29 +0100

    Update NucleoFeature.R

--- NucleoFeature.R
+++ NucleoFeature.R
@@ -7,31 +7,30 @@ library(NucleoFeature)
 ui <- fluidPage(
   tags$style(HTML("
     .my-custom-button {
-      background-color: #4499ff !important; /* Custom orange color */
-      color: white !important; /* White text */
-      border-radius: 10px; /* Rounded corners */
+      background-color: #4499ff;
+      color: white;
+      border-radius: 10px;
       font-size: 16px;
       font-weight: bold;
       padding: 10px 20px;
       border: none;
     }
     .my-custom-button:hover {
-      background-color: #44aa19 !important; /* Darker color on hover */
+      background-color: #44aa19 ;
     }
   ")),
-  #theme = shinythemes::shinytheme("superhero"),
-  titlePanel("Nucleotide Sequence Analysis"),
+ # theme = shinythemes::shinytheme("superhero"),
+  titlePanel("NucleoFeature: Shiny Interface for Nucleotide Sequence Analysis"),
 
   sidebarLayout(
     sidebarPanel(
       textAreaInput("sequence", "Enter DNA Sequence:",
-                    value = "ACGTACGTACGT", rows = 12),
+                    value = "TACGTACGTACGT", rows = 12),
       numericInput("kmer", "Select k-mer size:", value = 2, min = 1, max = 6),
       actionButton("analyze", "Analyze",class = "my-custom-button"),
       hr(),
       fileInput("file", "Upload Sequence File (.txt)", accept = ".txt"),
-      downloadButton("downloadData", "Download Results as CSV",class = "my-custom-button")  # <-- Download Button in UI
-    ),
+      downloadButton("downloadData", "Download Results as CSV",class = "my-custom-button")    ),
 
 
     mainPanel(
@@ -50,6 +49,11 @@ ui <- fluidPage(
       tableOutput("kmerTable"),
       h2("Pseudo K-tuple Nucleotide Composition (PseKNC)"),
       tableOutput("psekncTable"),
+      h2("Reverse Complement and Kmer"),
+
+      verbatimTextOutput("reverse_complement_output"),
+
+      tableOutput("kmer_compositionreverse"),
       h2("Structural Features"),
 
       h4("DNA Stability Index"),
@@ -78,9 +82,7 @@ ui <- fluidPage(
       h4("DNA Bending Stiffness"),
       verbatimTextOutput("BendingStiffness"),
 
-      h2("Motif-Based Features"),
 
-      h4("Transcription Factor Binding Sites"),
       tableOutput("BindingSites"),
 
 
@@ -95,10 +97,12 @@ ui <- fluidPage(
 
 
           )
-  )
+
+  ),
+ HTML("<hr><p style='text-align:center; font-weight:bold;'>
+       Developed by Lopamudra Dey | © 2025</p>")
 )
 
-# Empty server function
 server <- function(input, output) {
 
   analyze_sequence <- reactive({
@@ -106,16 +110,16 @@ server <- function(input, output) {
     seq_input <- input$sequence  # Get user input
 
     if (!is.null(input$file)) {
-      # Read file if uploaded
       seq_input <- readLines(input$file$datapath)
       seq_input <- paste(seq_input, collapse = "")
     }
 
-    # Compute features using your package functions
-    comp_result <- ACTG_composition(seq_input)  # Nucleotide composition
-    gc_result <- gc_content(seq_input)  # GC content
-    entropy_result <- shannon_entropy(seq_input)  # Sequence entropy
+    comp_result <- ACTG_composition(seq_input)  
+    gc_result <- gc_content(seq_input)  
+    entropy_result <- shannon_entropy(seq_input)  
     kmerTable <- kmer_frequency(seq_input, input$kmer)
+    reverse_complement_output<-reverse_seqcomplement(seq_input)
+    kmer_compositionreverse<-kmer_compositionreverse(seq_input, input$kmer)
     psekncTable<-pseknc_composition(seq_input, input$kmer)# k-mer frequencies
     stabilityIndex<-compute_stability(seq_input)
     zDnaProbability<-compute_z_dna_probability(seq_input)
@@ -126,7 +130,7 @@ server <- function(input, output) {
     Structure<-compute_physicochemical_properties(seq_input)
     Nucleosome<-compute_nucleosome_positioning(seq_input)
     BendingStiffness<-compute_bending_stiffness(seq_input)
-    BindingSites<-detect_tfbs(seq_input)
+   # BindingSites<-detect_tfbs(seq_input)
     CpGIslands<-detect_cpg_island(seq_input)
     #DNAConserved<-detect_conserved_elements(seq_input)
     return(list(
@@ -135,6 +139,8 @@ server <- function(input, output) {
       entropy = entropy_result,
       kmerTable = kmerTable,
       psekncTable=psekncTable,
+      reverse_complement_output=reverse_complement_output,
+      kmer_compositionreverse=kmer_compositionreverse,
       stabilityIndex= stabilityIndex,
       zDnaProbability=zDnaProbability,
       curvature=curvature,
@@ -144,28 +150,28 @@ server <- function(input, output) {
       Structure=Structure,
       Nucleosome=Nucleosome,
       BendingStiffness=BendingStiffness,
-      BindingSites=BindingSites,
+      #BindingSites=BindingSites,
       CpGIslands=CpGIslands
       #DNAConserved=DNAConserved
     ))
   })
 
-  # Output: Nucleotide Composition Table
   output$composition <- renderTable({
     analyze_sequence()$composition
   })
 
-  # Output: GC Content
   output$gcContent <- renderPrint({
     analyze_sequence()$gc_content
   })
 
-  # Output: Shannon Entropy
   output$entropy <- renderPrint({
     analyze_sequence()$entropy
   })
   output$psekncTable <- renderTable({
     analyze_sequence()$ psekncTable
+  })
+  output$reverse_complement_output <- renderText({
+    analyze_sequence()$reverse_complement_output
   })
   output$stabilityIndex <- renderPrint({
     analyze_sequence()$stabilityIndex
@@ -197,56 +203,31 @@ server <- function(input, output) {
     analyze_sequence()$BendingStiffness
   })
 
-
-  output$BindingSites <- renderTable({
-    binding <- analyze_sequence()$BindingSites  # Extract binding sites data
-
-    # Debugging: Print structure
-    print(str(binding))
-
-    # Check if it's a named numeric vector
-    if (is.null(binding) || length(binding) == 0) {
-      return(data.frame(BindingSite = "No data", Frequency = 0))
-    }
-
-    if (is.numeric(binding) && !is.data.frame(binding)) {
-      binding <- data.frame(BindingSite = names(binding), Frequency = as.numeric(binding))
-    }
-
-    # Ensure there are no NA values
-    binding[is.na(binding)] <- 0
-
-    return(binding)  # Return properly formatted data frame
+  output$kmer_compositionreverse <- renderTable({
+    analyze_sequence()$kmer_compositionreverse
   })
-
-
-
 
     output$CpGIslands<- renderPrint({
       analyze_sequence()$CpGIslands
   })
 
 
-  # Output: K-mer Frequency Table
     output$kmerTable <- renderTable({
-      kmer_data <- analyze_sequence()$kmerTable  # Extract k-mer frequencies
+      kmer_data <- analyze_sequence()$kmerTable  
 
-      # Debugging: Check structure
       print(str(analyze_sequence()))
       print(head(analyze_sequence()$kmerTable))
 
 
-      # Handle NULL or incorrectly formatted data
       if (is.null(kmer_data) || nrow(kmer_data) == 0) {
         return(data.frame(Kmer = "No data", Frequency = 0))
       }
 
-      # Ensure the data frame has proper column names
       if (!("Kmer" %in% colnames(kmer_data)) || !("Frequency" %in% colnames(kmer_data))) {
         return(data.frame(Kmer = "Error", Frequency = "NA"))
       }
 
-      return(kmer_data)  # Directly return data frame
+      return(kmer_data)
     })
 
 
@@ -259,32 +240,7 @@ server <- function(input, output) {
             col = "skyblue", main = "Nucleotide Composition")
   })
 
- # output$kmerPlot <- renderPlot({
- #   kmer_data <- analyze_sequence()$kmer_freq
 
-
-    # Ensure k-mer data is structured properly
- #   kmer_df <- data.frame(Kmer = names(kmer_data), Frequency = as.numeric(kmer_data))
-
-    # Handle missing or zero values
- #   kmer_df$Frequency[is.na(kmer_df$Frequency)] <- 0
-
-    # Prevent empty plot issue
-    #if (all(kmer_df$Frequency == 0)) {
-  #    plot.new()
-  #    text(0.5, 0.5, "All k-mer frequencies are zero!", cex = 1.5, col = "red")
-  #    return()
-  #  }
-
-    # Plot the k-mer frequencies
- #   barplot(kmer_df$Frequency,
-      #      names.arg = kmer_df$Kmer,
-      #      col = "lightgreen",
-      #      las = 2,  # Rotate labels for better visibility
-      #      ylim = c(0, max(kmer_df$Frequency, na.rm = TRUE) + 5),  # Adjust ylim dynamically
-      #      main = paste(input$kmer, "-mer Frequency"),
-        #    ylab = "Frequency (%)",
-       #     xlab = "K-mers")
  # })
 
   output$downloadData <- downloadHandler(
@@ -292,13 +248,15 @@ server <- function(input, output) {
       paste("sequence_analysis_results", Sys.Date(), ".csv", sep = "")
     },
     content = function(file) {
-      # Retrieve the analysis results
+
       results_list <- analyze_sequence()
+      composition_df <- as.data.frame(t(results_list$composition))
 
-      # Convert composition (which may be a named vector or data frame) to a data frame
-      composition_df <- as.data.frame(t(results_list$composition))  # Transpose for CSV format
-
-      # Create a data frame with other results
+     
+      reverse_complement_str <- ifelse(is.null(results_list$reverse_complement_output),
+                                       NA,
+                                       paste(results_list$reverse_complement_output, collapse = "; "))
+      #df2<- as.data.frame(t(results_list$kmer_compositionreverse))
       results <- data.frame(
         GC_Content = results_list$gc_content,
         Shannon_Entropy = results_list$entropy,
@@ -310,19 +268,17 @@ server <- function(input, output) {
         Hydrophobicity = results_list$Structure,
         Nucleosome_Positioning = results_list$Nucleosome,
         BendingStiffness= results_list$BendingStiffness,
-        BindingSites=results_list$BindingSites,
-        CpGIslands=results_list$CpGIslands
-
+        CpGIslands=results_list$CpGIslands,
+       reverse_complement_output=reverse_complement_str
 
       )
 
-      # Merge both data frames
       final_results <- cbind(composition_df, results)
+      str(composition_df)
 
-      # Write the complete data frame to CSV
       write.csv(final_results, file, row.names = FALSE)
     }
   )
 }
-# Run the application
+
 shinyApp(ui = ui, server = server)
